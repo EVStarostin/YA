b712842e193e6f699f9efd8f9fbbcfc4a80b8a86
Evgenii Starostin <e.starostin89@gmail.com>
2018-10-05 02:12:24 +0300

    +

--- home_work_1/js/main.js
+++ home_work_1/js/main.js
@@ -113,6 +113,7 @@ function setMaxHeightForTruncate() {
     truncatedStrings.forEach(item => {
       const maxHeight = parseFloat(getComputedStyle(item).lineHeight) * 2;
       if (item.scrollHeight - maxHeight > 1) item.classList.add('event__title_truncated');
+      item.innerText = item.scrollHeight - maxHeight;
       item.style.maxHeight = `${maxHeight}px`;
     });
   }, 100);
