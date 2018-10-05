13db836ea591e69d9e23e41a79f59c104a5991eb
Evgenii Starostin <e.starostin89@gmail.com>
2018-10-05 20:55:45 +0300

    +

--- home_work_2/src/gestures/pinch.js
+++ home_work_2/src/gestures/pinch.js
@@ -4,12 +4,20 @@ setTimeout(handlePinch, 1000);
 
 function handlePinch(e) {
   let gestures = {
-    gests: [],
-    startDistance: 0
+    arr: [
+      // {
+      //   pointerId: 666,
+      //   startX: 100,
+      //   prevX: 100,
+      //   prevTs: Date.now(),
+      // }
+    ],
+    startZoom: null,
+    initialDistance: null
   }
 
   const nodeState = {
-    startDistance: 0
+    startZoom: 1
   };
 
   const frame = document.querySelector('.event__pic-img');
@@ -18,33 +26,52 @@ function handlePinch(e) {
   function pointerDownHandler(e) {
     frame.setPointerCapture(e.pointerId);
 
-    gestures.startZoom = nodeState.startDistance;
-    gestures.gests.push({
+    gestures.startZoom = nodeState.startZoom;
+    gestures.arr.push({
       pointerId: e.pointerId,
       startX: e.x,
       prevX: e.x,
       prevTs: Date.now(),
     });
+
+    if (gestures.arr.length === 2) {
+      gestures.initialDistance = Math.abs(gestures.arr[0].prevX - e.x);
+    } else {
+      gestures.initialDistance = gestures.arr.join('; ');
+    }
     
     frame.addEventListener('pointermove', pointerMoveHandler);
   }
 
   function pointerMoveHandler(e) {
-    if (gestures.gests.length !== 2) {
+    if (gestures.arr.length !== 2) {
       return;
     };
 
-    const curGest = gestures.gests.find(gest => gest.pointerId === e.pointerId);
-    const secondGest = gestures.gests.find(gest => gest !== curGest);
-    console.log(Math.abs(secondGest.prevX - e.x));
+    const curGest = gestures.arr.find(gest => gest.pointerId === e.pointerId);
+    const secondGest = gestures.arr.find(gest => gest !== curGest);
+    console.log(Math.abs( (secondGest.prevX - curGest.prevX) - (secondGest.prevX - curGest.prevX - e.x) ));
     const distance = Math.abs(secondGest.prevX - e.x);
-    document.querySelector('.event__pic-img').style.backgroundSize = `${100 + distance / 100}%`;
+    const distanceDif = distance - gestures.initialDistance;
+    const zoom = gestures.startZoom + (distanceDif / 100);
+    results.innerText = `
+      curGest: prevX = ${curGest.prevX} | x = ${e.x}
+      secondGest = ${secondGest.prevX}
+      distance = ${distance}
+      zoom = ${zoom}
+      initialDistance = ${gestures.initialDistance}
+      ${Math.abs( (secondGest.prevX - curGest.prevX) - (secondGest.prevX - curGest.prevX - e.x) )}
+    `;
+
+    document.querySelector('.event__pic-img').style.backgroundSize = `${zoom * 100}%`;
+    nodeState.startZoom = zoom;
+    console.log('startZoom', gestures.startZoom);
 
     curGest.prevX = e.x;
   }
 
   /* При поднятии пальца удалять событие из массива. */
   frame.addEventListener('pointerup', (e) => {
-    gestures.gests = gestures.gests.filter(gest => gest.pointerId !== e.pointerId);
+    gestures.arr = gestures.arr.filter(gest => gest.pointerId !== e.pointerId);
   });
 }
