ddbd586076db829e4ac2997589d37701108d405d
Evgenii Starostin <e.starostin89@gmail.com>
2018-10-07 02:59:19 +0300

    +

--- home_work/src/js/constants/index.js
+++
@@ -1,8 +0,0 @@
-export const INITIAL_ZOOM = 200;
-export const MAX_ZOOM = 1000;
-export const MIN_ZOOM = 100;
-export const ZOOM_SPEED = 30;
-export const INITIAL_BRIGHTNESS = 100;
-export const MIN_BRIGHTNESS = 0;
-export const MAX_BRIGHTNESS = 500;
-export const INITIAL_SCROLL = 0;
